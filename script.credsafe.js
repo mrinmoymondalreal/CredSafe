de069a98bc4d709fc777cb5d6c9f63c2b760c2f9
Mrinmoy Mondal <mmtech6100@gmail.com>
2023-04-21 15:02:09 +0530

    modified script of credsafe

--- script.credsafe.js
+++ script.credsafe.js
@@ -66,7 +66,7 @@
             }
         }`;
     document.head.appendChild(style);
-    document.body.innerHTML = document.body.innerHTML + `<div class="bg_credsafe">
+    document.body.innerHTML = `<div class="bg_credsafe">
     <div class="model">
         <div class="scr" style="display: none;flex-direction: column;">
             <img class="cred_qr" width="300" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAHQAAAB0CAYAAABUmhYnAAAAAklEQVR4AewaftIAAAKdSURBVO3BQY7cQAwEwSxC//9yeo48NSBIs17TjIgfrDGKNUqxRinWKMUapVijFGuUYo1SrFGKNUqxRinWKMUapVijFGuUYo1y8VASfpLKHUnoVLokdCpdEn6SyhPFGqVYoxRrlIuXqbwpCb+JypuS8KZijVKsUYo1ysWXJeEOlTuS0Kn8pCTcofJNxRqlWKMUa5SLYZLQqXRJmKxYoxRrlGKNcjGMyolKl4RJijVKsUYp1igXX6byNyXhROUJld+kWKMUa5RijXLxsiT8ZipdEjqVkyT8ZsUapVijFGuU+ME/LAknKv+TYo1SrFGKNcrFQ0noVLokvEmlU7kjCZ3KSRLepPJNxRqlWKMUa5SLL1PpknCHykkSOpU7kvA3JaFTeaJYoxRrlGKNEj94URKeUOmScKLSJaFTOUnCHSonSbhD5U3FGqVYoxRrlIuHktCpdEnoVLoknKicJOGOJNyhcpKEO1S+qVijFGuUYo0SP3hREk5U7khCp/KmJNyh8qYkdCpPFGuUYo1SrFHiB/+wJHQqXRI6lS4JncoTSehUflKxRinWKMUaJX7wQBJ+ksoTSehUTpLQqdyRhBOVNxVrlGKNUqxRLl6m8qYknCShUzlROUlCp9IloVPpknBHEjqVJ4o1SrFGKdYoF1+WhDtUvikJnUqn0iXhDpU7kvCmYo1SrFGKNcrFMEk4UemS8EQSTlROVN5UrFGKNUqxRrkYTuUOlZMkdConSehUvqlYoxRrlGKNcvFlKt+kcpKETuUkCZ1Kp9Il4Tcp1ijFGqVYo1y8LAk/KQmdykkSTlS6JHQqJyonSehU3lSsUYo1SrFGiR+sMYo1SrFGKdYoxRqlWKMUa5RijVKsUYo1SrFGKdYoxRqlWKMUa5RijfIHfREA5mJl58EAAAAASUVORK5CYII=" alt="">
@@ -77,7 +77,7 @@
             <div class="loading-text">Loading</div>
         </div>
     </div>
-</div>`;
+</div>` + document.body.innerHTML;
     document.body.insertBefore(script, document.body.lastElementChild);
   }
   function isDef(a){
